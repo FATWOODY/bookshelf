d52ff29ff4a7762a498a6bc3b62c7022f3222592
Michael <154087850+FATWOODY@users.noreply.github.com>
2025-07-04 22:51:13 -0500

    Fix NextAuth MissingSecret Error

--- app/auth.ts
+++ app/auth.ts
@@ -3,5 +3,6 @@ import Google from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   debug: true,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [Google],
 });
